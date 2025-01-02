826b12b3532ab7b222119d1655bb337d78d2a203
Laura Echevarria <lauraeche12@gmail.com>
2025-01-02 11:19:14 +0100

    Updates

--- src/services/games.js
+++
@@ -1,6 +0,0 @@
-import games from '../data/games.json';
-
-export const getGame = (id) => {
-  let test = games.filter((game) => game.id === id)[0];
-  return test;
-};
--- src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-const IS_LOGGED = 'isLogged';
-
-export const getIsLogged = () => {
-  return localStorage.getItem(IS_LOGGED);
-};
-
-export const setIsLogged = (isLogged) => {
-  localStorage.setItem(IS_LOGGED, isLogged);
-};
